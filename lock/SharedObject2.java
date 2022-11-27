4c438d2c2d5deb2ca49959670f8f8068929e2a3d
alekoChe <aleko2117bis@gmail.com>
2022-11-27 12:32:59 +0300

    delete1

--- lock/SharedObject2.java
+++ lock/SharedObject2.java
@@ -3,10 +3,4 @@ package lock;
 public class SharedObject2 {
     int counter = 0;
 
-//    public void incrementCounter() {
-//        counter++;
-//    }
-//    public void decrementCounter() {
-//        counter--;
-//    }
 }
